c3394c7c055e8b1f0baae82f5bdcecac0ed5edd0
cloudred <cloudred442@gmail.com>
2025-05-15 13:01:56 +0800

    newest commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { PassportModule } from "@nestjs/passport";
+import { SupabaseStrategy } from "../common/strategies/supabase.strategy";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
-import { SupabaseStrategy } from "./strategies/supabase.strategy";
 
 @Module({
   imports: [PassportModule, ConfigModule],
